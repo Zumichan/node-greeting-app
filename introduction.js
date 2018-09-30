c0718abc37ffece9db59e30fb71b307500ef19b1
Azumi Callanan <azumi@Azumis-MacBook.local>
2018-09-30 16:23:00 -0700

    Assignment Basic Node App: Completed

---
+++ introduction.js
@@ -0,0 +1,7 @@
+exports.introductionJapanese = function(){
+ console.log("Hajimemashite");
+}
+
+exports.introductionEnglish = function(){
+ console.log("Nice to meet you");
+}
---
+++ main.js
@@ -0,0 +1,20 @@
+const greetings = require("./greeting.js");
+const introduction = require("./introduction.js");
+
+// "Hello"
+greetings.greetingEnglish();
+
+// "Konnichiwa"
+greetings.greetingJapanese();
+
+//"Hola"
+greetings.greetingSpanish();
+
+//"Halló"
+greetings.greetingIcelandic();
+
+//"Nice to meet you"
+introduction.introductionEnglish();
+
+//"Hajimemashite"
+introduction.introductionJapanese();
